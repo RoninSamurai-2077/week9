df1c8ddaa71aec8c8c6950f02ac0f2662b8b7deb
Greg <Gdixon5125@gmail.com>
2021-07-19 12:25:39 -0400

    umm

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+app.use('view engine', 'ejs');
+app.get('/:userQuery', (req, res) => {
+    res.render('index',{data : {userQuery: req.params.userQuery}});
 })
  
 app.listen(3000)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
