b65d5c8686518d557d5d6a0f667abfbdf11dbfea
Greg <Gdixon5125@gmail.com>
2021-07-19 14:23:03 -0400

    yes i did it

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.set('view engine', 'ejs');
 app.get('/:userQuery', (req, res) => {
     res.render('index',{data : {userQuery: req.params.userQuery}});
 })
-
+Server.listen(5000)
 app.listen(3000);
 let port = 3000;
     console.log(`Server is listening on ${port}`);
